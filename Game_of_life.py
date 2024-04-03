e3f0632697809872571696d64211dd762b707144
Gabriel MONNIER <g.monnier@saint-louis29.net>
2024-04-03 09:25:02 +0200

    ajout de la fonction get voisin qui marche et la fonction tour qui est en construction

--- Game_of_life.py
+++ Game_of_life.py
@@ -1,1 +1,90 @@
-print("hello")
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+
+class Jeudelavie:
+    def __init__(self, hauteur, largeur):
+        """
+        Affecte un tableau à deux dimensions à l’attribut tableau
+
+        :param tableau: tableau à deux dimensions
+        0.0 = cellule morte
+        1.0 = cellule vivante
+        """
+        self.largeur = largeur
+        self.hauteur = hauteur
+        self.tab = np.zeros((hauteur, largeur))
+        
+    
+    def tour(self):
+        """
+        Met à jour toutes les cellules du tableau en respectant les règles
+        du jeu de la vie.
+        """
+        for h in range (self.hauteur):
+            for l in range (self.largeur):
+                voisins = get_voisins(h,l)
+                if self.tab[h][l] == 1.0 and voisins == 2 or voisins == 3:
+                    self.tab[h][l] = 1.0
+                elif self.tab[h][l] == 0.0 and voisins == 3:
+                    self.tab[h][l] = 1.0
+                else:
+                    self.tab[h][l] = 0.0
+        
+        
+        
+    def get_voisins(self, hauteur, largeur):
+        """
+        renvoie le nombre de voisins d'une cellule
+        """
+        sous_matrice = self.tab[hauteur-1:hauteur+2,largeur-1:largeur+2]
+        if self.tab[hauteur][largeur] == 1.0:
+            return np.sum(sous_matrice)-1
+        else:
+            return np.sum(sous_matrice)
+    
+    
+
+
+########## test de "get_voisins" #################
+
+g = Jeudelavie(5,5)        
+        
+expected = 0
+result = g.get_voisins(2,3)
+assert result == expected, f"le resultat devrait être {expected} mais vaut {result}."
+
+g.tab[2][4] = 1
+expected = 0
+result = g.get_voisins(2,4)
+assert result == expected, f"le resultat devrait être {expected} mais vaut {result}."
+
+g.tab[3][4] = 1
+g.tab[1][3] = 1
+expected = 2
+result = g.get_voisins(2,4)
+assert result == expected, f"le resultat devrait être {expected} mais vaut {result}."
+
+########## test de "tour" #################
+
+
+g = Jeudelavie(2,2)        
+        
+# expected = 0
+# result = g.get_voisins(2,3)
+# assert result == expected, f"le resultat devrait être {expected} mais vaut {result}."
+# 
+# g.tab[2][4] = 1
+# expected = 0
+# result = g.get_voisins(2,4)
+# assert result == expected, f"le resultat devrait être {expected} mais vaut {result}."
+# 
+# g.tab[3][4] = 1
+# g.tab[1][3] = 1
+# expected = 2
+# result = g.get_voisins(2,4)
+# assert result == expected, f"le resultat devrait être {expected} mais vaut {result}."
+# 
+
+
---
+++ setup.py
@@ -0,0 +1,23 @@
+import os
+
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "game_of_life",
+    version = "0.0.1",
+    author = "Gabriel Monnier",
+    author_email = "monnier-g@saint-louis29.net",
+    description = ("un programme du jeu de la vie "),
+    license = "BSD",
+    keywords = "example documentation tutorial",
+    url = "http://packages.python.org/an_example_pypi_project",
+    packages=['an_example_pypi_project', 'tests'],
+    long_description=read('README'),
+)
